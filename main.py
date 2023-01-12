5e0376d3e65bde76739d5df7cc5981f931baaf92
Zerak630 <zerak.630@outlook.fr>
2023-01-12 10:02:04 +0100

    Remove artists router & config

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from dotenv import dotenv_values
 from pymongo import MongoClient
 from neo4j import GraphDatabase, basic_auth
 
-from router_artist import router as artist_router
 from router_movie import router as movies_router
 
 config = dotenv_values(".env")
@@ -14,36 +13,18 @@ app = FastAPI()
 def startup_db_client():
     
     print("Connecting to MongoDB database...")
-    match config["MONGO_DB_SWITCH"]:
-        case "Local":
-            app.mongodb_client = MongoClient(config["LOCAL_URI"])
-            app.database = app.mongodb_client[config["LOCAL_DB_NAME"]]
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["ATLAS_DB_NAME"]]
 
-            app.include_router(artist_router, tags=["artists"], prefix="/artist")
+    app.include_router(movies_router, tags=["movies"], prefix="/movie")
 
-            print("Connected to the local MongoDB database!")
+    print("Connected to the Atlas personal MongoDB database!")
 
-        case "Atlas":
-            app.mongodb_client = MongoClient(config["ATLAS_URI"])
-            app.database = app.mongodb_client[config["ATLAS_DB_NAME"]]
-
-            app.include_router(movies_router, tags=["movies"], prefix="/movie")
-
-            print("Connected to the Atlas personal MongoDB database!")
-    
-        case "Paco":
-            app.mongodb_client = MongoClient(config["PACO_URI"])
-            app.database = app.mongodb_client[config["PACO_DB_NAME"]]
-
-            app.include_router(movies_router, tags=["movies"], prefix="/movie")
-
-            print("Connected to the Atlas Paco's MongoDB database!")
-    
     print("Connecting to Neo4j database...")
 
     app.neo_client = GraphDatabase.driver(
-    config["NEO4J_URI"],
-    auth=basic_auth(config["NEO4J_USER"], config["NEO4J_PASSWORD"]))
+        config["NEO4J_URI"],
+        auth=basic_auth(config["NEO4J_USER"], config["NEO4J_PASSWORD"]))
 
     print("Connected to Neo4j database!")
 
